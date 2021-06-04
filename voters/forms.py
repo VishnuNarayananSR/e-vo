64a0b0bd9fbe254de0e77d0386d08fa91fd6b133
CyanFroste <cyan14froste@gmail.com>
2021-06-04 13:47:49 +0530

    cyan v3

--- voters/forms.py
+++ voters/forms.py
@@ -1,11 +1,41 @@
 from django import forms
+
+
 class VoterForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=50, widget= forms.TextInput(attrs={'class':'form-control mb-3'}))
-    voter_id = forms.IntegerField(label='Voter Id', widget= forms.TextInput(attrs={'class':'form-control mb-3'}))
-    constituency = forms.CharField(label='Constituency', max_length=50, widget= forms.TextInput(attrs={'class':'form-control mb-3'}))
-    
+    name = forms.CharField(
+        label="Name",
+        max_length=50,
+        widget=forms.TextInput(attrs={"class": "form-control mb-3"}),
+    )
+    voter_id = forms.IntegerField(
+        label="Voter Id", widget=forms.TextInput(attrs={"class": "form-control mb-3"})
+    )
+    constituency = forms.CharField(
+        label="Constituency",
+        max_length=50,
+        widget=forms.TextInput(attrs={"class": "form-control mb-3"}),
+    )
+
+
 class VoteForm(forms.Form):
-    voter_id = forms.IntegerField(label='Voter Id', widget= forms.TextInput(attrs={'class':'form-control mb-3'}))
-    constituency = forms.CharField(label='Constituency', max_length=50, widget= forms.TextInput(attrs={'class':'form-control mb-3'}))
-    vote_for = forms.CharField(label='Vote for', max_length=50, widget= forms.TextInput(attrs={'class':'form-control mb-3'}))
-    symbol = forms.CharField(label='Vote Symbol' , widget= forms.TextInput(attrs={'class':'form-control mb-3'}))
+    voter_id = forms.IntegerField(
+        label="Voter Id", widget=forms.TextInput(attrs={"class": "form-control mb-3"})
+    )
+    constituency = forms.CharField(
+        label="Constituency",
+        max_length=50,
+        widget=forms.TextInput(attrs={"class": "form-control mb-3"}),
+    )
+    vote_for = forms.CharField(
+        label="",
+        max_length=50,
+        widget=forms.HiddenInput(
+            attrs={"class": "form-control mb-3", "id": "candidate-input"}
+        ),
+    )
+    symbol = forms.CharField(
+        label="",
+        widget=forms.HiddenInput(
+            attrs={"class": "form-control mb-3", "id": "symbol-input"}
+        ),
+    )
--- voters/views.py
+++ voters/views.py
@@ -5,43 +5,42 @@ import utils
 
 
 def voter_registration(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = VoterForm(request.POST)
-        name = form['name'].value()
-        voter_id = form['voter_id'].value()
-        constituency = form['constituency'].value()
+        name = form["name"].value()
+        voter_id = form["voter_id"].value()
+        constituency = form["constituency"].value()
         if form.is_valid():
             contract, tx_details, exceptions = utils.contract()
             try:
                 contract.createVoter(name, voter_id, constituency, tx_details)
             except exceptions.VirtualMachineError as e:
                 return HttpResponse(e.revert_msg)
-            return HttpResponse('Voter Registered.')
+            return HttpResponse("Voter Registered.")
     else:
-         form = VoterForm()
-    return render(request, 'voter_registration.html', {'form':form})
+        form = VoterForm()
+        return render(request, "voter_registration.html", {"form": form})
+
 
 def vote(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = VoteForm(request.POST)
-        voter_id = form['voter_id'].value()
-        constituency = form['constituency'].value()
-        
-        vote_for = form['vote_for'].value()
-        symbol = form['symbol'].value()
+        voter_id = form["voter_id"].value()
+        constituency = form["constituency"].value()
+
+        vote_for = form["vote_for"].value()
+        symbol = form["symbol"].value()
 
         if form.is_valid():
             contract, tx_details, exceptions = utils.contract()
-
-            candidates = contract.getCandidatesList(tx_details)
-            candidates = list(set(map(lambda c: (c[0], c[2]), candidates)))
-
             try:
                 contract.vote(voter_id, vote_for, constituency, symbol, tx_details)
             except exceptions.VirtualMachineError as e:
                 return HttpResponse(e.revert_msg)
-            return HttpResponse('Voted Successfully.')
+            return HttpResponse("Voted Successfully.")
     else:
-         form = VoteForm()
-    return render(request, 'vote.html', {'form':form})
-
+        contract, tx_details, exceptions = utils.contract()
+        candidates = contract.getCandidatesList()  # ?
+        candidates = list(set(map(lambda c: (c[0], c[2]), candidates)))
+        form = VoteForm()
+        return render(request, "vote.html", {"form": form, "candidates": candidates})
