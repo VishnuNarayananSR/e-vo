724d193fea13ed745cdafc4c4043c2259510a262
Vishnu Narayanan S R <srvishnunarayanan@gmail.com>
2021-07-11 12:12:37 +0530

    face_auth untidy

--- evo/urls.py
+++ evo/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.homepage, name='home'),
     path('candidates/', include('candidates.urls')),
-    path('voters/', include('voters.urls'))
+    path('voters/', include('voters.urls')),
+    # path('cam/', views.cam_page),
+    path("cam/", views.cam_stream),
+    path('signal/', views.cam_stream, name="video_stream")
 ]
 urlpatterns += staticfiles_urlpatterns()
